aa238be4663dd43c0da0eae2ee79c840cc68ff5c
Philip <philip.robinson@globalfishingwatch.org>
2024-12-03 00:04:36 +0000

    commit updates for renv and docker

--- docker-env.sh
+++ docker-env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cat <<EOF > .env
 UID=$(id -u)
 GID=$(id -g)
--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -euo pipefail
+
 ./docker-env.sh
 DOCKER_BUILDKIT=1 docker compose up dev
