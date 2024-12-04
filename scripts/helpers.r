67ef19d6e014a3dfda4056dd64603470b81fe165
Philip <philip.robinson@globalfishingwatch.org>
2024-12-04 19:10:50 +0000

    safe table strategy

--- scripts/helpers.r
+++ scripts/helpers.r
@@ -7,53 +7,63 @@ library(bigrquery)
   if (!is.null(x)) x else y
 }
 
-config <- yaml::read_yaml("config.yaml")
-
 safe_table_parts <- function(
     table,
-    project = config$safe_table_get$project,
-    dataset = config$safe_table_get$dataset,
-    scratch = config$safe_table_get$scratch,
-    scratch_location = config$safe_table_get$scratch_location) {
+    project = NULL,
+    dataset = NULL,
+    scratch = NULL,
+    scratch_location = NULL) {
+  config <- yaml::read_yaml("./scripts/config.yaml")
+
   # Look for a table-specific configuration
   table_config <- purrr::keep(config$safe_table_get$tables, ~ .x$name == table)
 
   if (length(table_config) > 0) {
-    # Override defaults with table-specific settings if found
+    # Override defaults with table-specific settings if null
     table_config <- table_config[[1]]
-    project <- table_config$project %||% project
-    dataset <- table_config$dataset %||% dataset
-    scratch <- table_config$scratch %||% scratch
-    scratch_location <- table_config$scratch_location %||% scratch_location
+    project <- project %||% table_config$project
+    dataset <- dataset %||% table_config$dataset
+    scratch <- scratch %||% table_config$scratch
+    scratch_location <- scratch_location %||% table_config$scratch_location
   }
 
-  dataset_name <- if (scratch) scratch_location else dataset
-  table_name <- if (scratch) paste0(dataset, "_", table) else table
+  # Override defaults with top level settings if null
+  project <- project %||% config$safe_table_get$project
+  dataset <- dataset %||% config$safe_table_get$dataset
+  scratch <- scratch %||% config$safe_table_get$scratch
+  scratch_location <-
+    scratch_location %||% config$safe_table_get$scratch_location
 
-  return(list(project = project, dataset = dataset_name, table = table_name))
+  # Override with scratch specific settings settings if scratch flag set
+  if (scratch) {
+    table <- paste0(dataset, "_", table)
+    dataset <- scratch_location
+  }
+
+  return(list(project = project, dataset = dataset, table = table))
 }
 
 safe_table_addr <- function(
     table,
-    project = config$safe_table_get$project,
-    dataset = config$safe_table_get$dataset,
-    scratch = config$safe_table_get$scratch,
-    scratch_location = config$safe_table_get$scratch_location) {
+    project = NULL,
+    dataset = NULL,
+    scratch = NULL,
+    scratch_location = NULL) {
   parts <- safe_table_parts(
     table,
     project = project, dataset = dataset,
     scratch = scratch, scratch_location = scratch_location
   )
-  addr <- paste(parts$project, parts$dataset, parts$table, sep = ".", " ")
+  addr <- paste(parts$project, parts$dataset, parts$table, sep = ".")
   return(addr)
 }
 
 safe_table_get <- function(
     table,
-    project = config$safe_table_get$project,
-    dataset = config$safe_table_get$dataset,
-    scratch = config$safe_table_get$scratch,
-    scratch_location = config$safe_table_get$scratch_location) {
+    project = NULL,
+    dataset = NULL,
+    scratch = NULL,
+    scratch_location = NULL) {
   parts <- safe_table_parts(
     table,
     project = project, dataset = dataset,
