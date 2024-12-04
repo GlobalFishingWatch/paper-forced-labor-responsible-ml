82b0ebe2a51b548f701723fdc3082aa39f0a3407
Philip <philip.robinson@globalfishingwatch.org>
2024-12-04 19:09:41 +0000

    docker updates

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,4 @@
 
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 docker pull --platform linux/amd64 rocker/tidyverse:4.4.2
-DOCKER_BUILDKIT=1 docker compose build dev
+DOCKER_BUILDKIT=1 docker compose build dev --ssh default=/Users/philiprobinson/.ssh/id_ed25519
--- docker-env.sh
+++ docker-env.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+rm -f .env
 cat <<EOF > .env
 UID=$(id -u)
 GID=$(id -g)
@@ -9,4 +10,6 @@ USERNAME=$(whoami)
 HOME=$(echo $HOME)
 CWD=$(realpath $(dirname $0))
 GITHUB_PAT=$(cat ./untracked/gittoken.json | jq ".token")
+SSH_AUTH_SOCK=$(echo $SSH_AUTH_SOCK)
 EOF
+chmod 600 .env
